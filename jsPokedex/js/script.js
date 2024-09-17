f01e36d0c5e100771b4a5c1312b5b8cd64a659df
Mardelen99 <jaimesmarlen5@gmail.com>
2024-09-16 22:11:14 -0500

    created a for loop that iterates over each item in the pokemonList using document.write

--- jsPokedex/js/script.js
+++ jsPokedex/js/script.js
@@ -1,1 +1,11 @@
-let pokemonList = [{name: "Bulbasaur", height: 7 , type: ["grass" ,"poison"]},{name: "Pikachu", height: 4, type:["electric","flying"]},{name: "Squirtle", height: 5 , type:["fire","ice"]}];
+let pokemonList = [{name: "Bulbasaur", height: 7 , type: ["grass" ,"poison"]},
+{name: "Pikachu", height: 4, type:["electric","flying"]},
+{name: "Squirtle", height: 5 , type:["fire","ice"]}];
+
+for (let i=0; i < pokemonList.length; i++) {
+        if (pokemonList[i].height <6 && pokemonList[i].height>0){
+        document.write(" "+ pokemonList[i].name + " (height: "+ pokemonList[i].height + ")")}
+        else if (pokemonList[i].height > 6) {
+            document.write(pokemonList[i].name + " (height: " + pokemonList[i].height +")"+ " - Wow, that's big! ");
+        }
+    }
