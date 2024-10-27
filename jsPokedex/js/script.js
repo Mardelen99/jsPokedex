4b1d588384ff33d86da5b1994fa760c8723017a5
Mardelen99 <jaimesmarlen5@gmail.com>
2024-10-27 18:27:15 -0500

    added IIFE (pokemonRepository , also added a getAll and add funtion , updated the for loop to forEach loop

--- jsPokedex/js/script.js
+++ jsPokedex/js/script.js
@@ -1,11 +1,35 @@
-let pokemonList = [{name: "Bulbasaur", height: 7 , type: ["grass" ,"poison"]},
-{name: "Pikachu", height: 4, type:["electric","flying"]},
-{name: "Squirtle", height: 5 , type:["fire","ice"]}];
-
-for (let i=0; i < pokemonList.length; i++) {
-        if (pokemonList[i].height <6 && pokemonList[i].height>0){
-        document.write(" "+ pokemonList[i].name + " (height: "+ pokemonList[i].height + ")")}
+   let pokemonList = [
+        { name: "Bulbasaur", height: 7, type: ["grass", "poison"] },
+        { name: "Pikachu", height: 4, type: ["electric", "flying"] },
+        { name: "Squirtle", height: 5, type: ["fire", "ice"] }
+    ];
+    
+    
+    const pokemonRepository = (function() {
+        const list = pokemonList; 
+    
+        function getAll() {
+            return list; 
+        }
+    
+        function add(item) {
+            if (item && typeof item === 'object') {
+                list.push(item); 
+            } else {
+                console.error('Invalid item: must be an object');
+            }
+        }
+    
+        return {
+            getAll: getAll,
+            add: add
+        };
+    })();
+    
+     for (let i=0; i < pokemonList.length; i++) {
+       if (pokemonList[i].height <6 && pokemonList[i].height>0){
+       document.write(" "+ pokemonList[i].name + " (height: "+ pokemonList[i].height + ")")}
         else if (pokemonList[i].height > 6) {
             document.write(pokemonList[i].name + " (height: " + pokemonList[i].height +")"+ " - Wow, that's big! ");
-        }
-    }
+       }
+   } 
